8b5cf999b062174bf1a6f1a6f28e4e56c1319da8
Lone <gyllone1993@gmail.com>
2023-12-06 01:20:38 +0800

    update

--- executor/base.py
+++ executor/base.py
@@ -1,19 +1,11 @@
 from abc import abstractmethod
-from typing import Any
+from typing import Any, List
 from pydantic import BaseModel, validator
 from langchain.output_parsers import PydanticOutputParser
 
-
-class BaseExecutor(BaseModel):
-    input: BaseModel
-    output: BaseModel
-from typing import Any, List
-from pydantic import BaseModel, validator
-from langchain.output_parsers import StructuredOutputParser, ResponseSchema
-
-
 class BaseExecutor(BaseModel):
     id: str
+    description: str
     input: List[BaseModel]
     output: List[BaseModel]
 
@@ -34,20 +26,6 @@ class BaseExecutor(BaseModel):
     def validate_output(cls, output: Any):
         if not isinstance(output, BaseModel):
             raise TypeError("output must be BaseModel")
-
-    @classmethod
-    @abstractmethod
-    def description(cls) -> str:
-        pass
-
-    def format_instruction(self) -> str:
-        pass
-        if isinstance(output, List):
-            for item in output:
-                if not isinstance(item, BaseModel):
-                    raise ValueError("Item must be pydantic model")
-        else:
-            raise ValueError("Output must be a list")
 
     def format_instruction(self) -> str:
         input_schema = [item.schema() for item in self.input]
